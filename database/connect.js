efe106bfdb7fa3d79f9b7a52e214c0d0c4901bb9
Konstantin Piterman <116492825+korlx@users.noreply.github.com>
2022-12-26 11:19:07 +0000

    121212

--- database/connect.js
+++ database/connect.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.userPass}@cluster0.ds3pany.mongodb.net/Toys`);
+  await mongoose.connect(`mongodb+srv://koko123:koko123123@cluster0.ds3pany.mongodb.net/?retryWrites=true&w=majority`);
    console.log("Connected to Toys DB")
  }
+ //mongodb+srv://${config.userDb}:${config.userPass}@cluster0.ds3pany.mongodb.net/Toys
--- routers/routeInit.js
+++ routers/routeInit.js
@@ -1,11 +1,11 @@
 const express=require('express')
 const app=express()
 const root=require("./root")
-const toys=require("./toys")
+
 const users=require("./users")
 exports.routeInit=(app)=>{
     app.use('/',root);
-    app.use("/toys",toys);
+ app.use('/users',users);
 
 
 
