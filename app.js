e3322ec12c58df951a81e11bf09a24595fd86d1c
Konstantin <kostyaaa2468@gmail.com>
2022-12-27 18:43:06 +0200

    dasdsad

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path=require('path');
+require("./database/connect")
+const {routeInit}=require('./routers/routeInit');
+
+const app = express()
+app.use(cors())
+const port = 3000
+
+app.use(express.json());
+routeInit(app);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+app.use(express.static(path.join(__dirname,'public',)));
